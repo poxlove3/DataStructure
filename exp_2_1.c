c50563177e865af74ebec0d68c7bab5126a50756
poxlove3 <524829578@qq.com>
2018-04-11 14:29:42 +0800

    commit message

--- exp_2_1.c
+++ exp_2_1.c
@@ -10,21 +10,95 @@ typedef struct BinaryTreeNode
     struct BinaryTreeNode* rChild;
 }BinaryTreeNode;
 
-BinaryTreeNode* PreCreateBinaryTree(BinaryTreeNode* BTNode);
-void PreOrderTransverse(BinaryTreeNode* BinaryTreeRoot);
+BinaryTreeNode* PreCreateBinaryTree(BinaryTreeNode* BTNode);//���򴴽� 
+void PreOrderTransverse(BinaryTreeNode* BinaryTreeRoot);//������� 
 BinaryTreeNode* InCreateBinaryTree(BinaryTreeNode* BTNode);
 void InOrderTransverse(BinaryTreeNode* BinaryTreeRoot);
 BinaryTreeNode* PostCreateBinaryTree(BinaryTreeNode* BTNode);
 void PostOrderTransverse(BinaryTreeNode* BinaryTreeRoot);
 
+int GetBTreeLeafNodesTotal(BinaryTreeNode* BinaryTreeRoot);//�ݹ鷽ʽ��Ҷ�ӽڵ���� 
+int GetBTreeNodesTotal(BinaryTreeNode* BinaryTreeRoot);//�ݹ鷽ʽ��ڵ����� 
+int GetBTeeeDepth(BinaryTreeNode* BinaryTreeRoot);
+
+BinaryTreeNode* Exchange(BinaryTreeNode* BinaryTreeNode);
+
 int main()
 {
     BinaryTreeNode* BTree;
-    BTree = InCreateBinaryTree(BTree);
+    BTree = PreCreateBinaryTree(BTree);
     PostOrderTransverse(BTree);
     printf("\n");
+    printf("%d\n",GetBTreeLeafNodesTotal(BTree));
+    printf("%d\n",GetBTreeNodesTotal(BTree));
+    printf("%d\n",GetBTeeeDepth(BTree));
+
+    BTree = Exchange(BTree);
+    PostOrderTransverse(BTree);
     return 0;
 }
+BinaryTreeNode* Exchange(BinaryTreeNode* BinaryTreeRoot)
+{
+    if(BinaryTreeRoot->lChild == NULL && BinaryTreeRoot->rChild == NULL)
+    {
+        return BinaryTreeRoot;
+    }
+    BinaryTreeNode* temp = NULL;
+    if(BinaryTreeRoot->lChild != NULL || BinaryTreeRoot->rChild != NULL)
+    {
+        BinaryTreeRoot->lChild = temp;
+        BinaryTreeRoot->lChild = BinaryTreeRoot->rChild;
+        BinaryTreeRoot->rChild = temp;
+    }
+    else
+    {
+        Exchange(BinaryTreeRoot->lChild);
+        Exchange(BinaryTreeRoot->rChild);
+    }
+}
+
+int GetBTeeeDepth(BinaryTreeNode* BinaryTreeRoot)
+{
+    int lDepth;
+    int rDepth;
+    if(BinaryTreeRoot == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        lDepth = GetBTeeeDepth(BinaryTreeRoot->lChild);
+        rDepth = GetBTeeeDepth(BinaryTreeRoot->rChild);
+    }
+
+    return (((lDepth > rDepth) ? lDepth : rDepth) + 1);
+}
+
+int GetBTreeLeafNodesTotal(BinaryTreeNode* BinaryTreeRoot)
+{
+	if(BinaryTreeRoot == NULL)
+    {
+        return 0;
+    }
+    if(BinaryTreeRoot->lChild == NULL && BinaryTreeRoot->rChild == NULL)
+    {
+        return 1;
+    }
+
+    return(GetBTreeLeafNodesTotal(BinaryTreeRoot->lChild) + GetBTreeLeafNodesTotal(BinaryTreeRoot->rChild));
+}
+
+int GetBTreeNodesTotal(BinaryTreeNode* BinaryTreeRoot)
+{
+	if(BinaryTreeRoot == NULL)
+    {
+        return 0;
+    }	
+    else
+    {
+        return(GetBTreeNodesTotal(BinaryTreeRoot->lChild) + GetBTreeNodesTotal(BinaryTreeRoot->rChild) + 1);
+    }
+} 
 
 BinaryTreeNode* PreCreateBinaryTree(BinaryTreeNode* BTNode)
 {
