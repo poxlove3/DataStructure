98866e40aedc6a2fb9724dd23d58dbf92409f327
poxlove3 <524829578@qq.com>
2018-05-25 21:39:06 +0800

    quichSort

--- exp4/exp1.c
+++ exp4/exp1.c
@@ -32,7 +32,7 @@ int findMin(List list,int startIndex)
 	return minIndex;
 }
 
-int swap(Entry *D,int i,int j) 
+int Swap(Entry *D,int i,int j) 
 {
 	if (i == j)
 	{
@@ -51,8 +51,77 @@ void easyChoseSelect(List *list)
 	while(startIndex < list->n - 1)
 	{
 		minIndex = findMin(*list,startIndex);
-		swap(list -> D,minIndex,startIndex);
+		Swap(list -> D,minIndex,startIndex);
 		startIndex++;
+	}
+}
+
+void bubbleSort(List *list)
+{
+	int i = 0;
+	int j = 0;
+	int n = list -> n;
+	int isSwap = 0;
+	for (j = n;j > 1;j--)
+	{
+		for(i = 0;i < j - 1;i++)
+		{
+			if (list -> D[i].key > list -> D[i + 1].key)
+			{
+				Swap(list -> D,i,i + 1);
+				isSwap = 1;
+			}
+		}
+		if (isSwap == 0)
+		{
+			break;
+		}
+	}
+}
+
+int Part(List *list,int low,int high)
+{
+	int i = low;
+	int j = high + 1;
+	Entry pivot = list -> D[low];
+	do
+	{
+		do {i++;}while(list -> D[i].key < pivot.key);
+		do {j--;}while(list -> D[i].key > pivot.key);
+
+		if (i < j)
+		{
+			Swap(list -> D,i,j);
+			//printf("%s\n", "yes");
+			/* code */
+		}
+	}while(i < j);
+	Swap(list -> D,low,j);
+	//printfArray(*list);
+	return j;
+}
+
+void quickSort(List *list,int low,int high)
+{
+	int k;
+	if (low < high)
+	{
+		k = Part(list,low,high);
+		printf("%d\n",k);
+		printf("%s\n", "yes");
+		quickSort(list,low, k - 1);
+		quickSort(list,k + 1,high);
+		/* code */
+	}
+}
+
+void printfArray(List list)
+{
+	int i = 0;
+	for (i = 0; i < list.n; i++)
+	{
+		printf("%d\n",list.D[i].key);
+		/* code */
 	}
 }
 
@@ -60,15 +129,20 @@ int main(int argc, char const *argv[])
 {
 	int i = 0;
 	List list;
-	list.n = 10;
-	for(i = 0;i < 10;i++)
+	int num = 0;
+	printf("%s\n","please input the number of list...");
+	scanf("%d",&num);
+	list.n = num;
+	for(i = 0;i < num;i++)
 	{
 		list.D[i].key = rand()%30;
 		printf("%d ",list.D[i].key);
 	}
 	printf("\n");
-	easyChoseSelect(&list);
-	for (i = 0; i < 10; ++i)
+	quickSort(&list,0,num - 1);
+	//bubbleSort(&list);
+	//easyChoseSelect(&list);
+	for (i = 0; i < num; i++)
 	{
 		printf("%d ",list.D[i].key);
 	}
